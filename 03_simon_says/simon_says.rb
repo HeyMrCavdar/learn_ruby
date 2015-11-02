9f455e803acf8aa2a6ebd2c43319eea1c73b1de2
Matt <matt.cavdar@gmail.com>
2015-11-02 17:36:04 -0500

    Add start of word method, simple string solution

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -15,15 +15,22 @@ def repeat(text, num = 2)
 	output.join(" ") #join takes a parameter, and combines an array into a string
 end
 
-def start_of_word(first, second)
-first.split[0]
+def start_of_word(string, num = 1)
+	string[0..num-1]
+
+	#This code is wrong!
+	# output = string.split(//)
+	# output[0..num-1]
+	# output.join
 end
 
 def first_word(word)
 end
 
 def titleize(word) #create an array of all words don't want to capitalize, use .map
+word.capitalize
 end
+
 
 
 
